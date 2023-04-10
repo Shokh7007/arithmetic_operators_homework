74d8527c69de422f62eac1b519d04e3047a094bf
Shokh7007 <shoxruxbeknafasov9@gmail.com>
2023-04-10 16:51:06 +0500

    3

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -3,5 +3,8 @@
 #Find the reverse of the number and assign it to a variable called 'answer'.
 
 #Print the value of the 'answer'
-
-
+number=21
+a=str(number//10)
+b=str(number%10)
+answer=b+a
+print(answer)
